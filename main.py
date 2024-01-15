386c2989f5a9c733a0531eb2dac8d32e6e1d8948
Caesar Robles <caesarrobles26@gmail.com>
2024-01-15 04:35:13 -0800

    added user input

--- main.py
+++ main.py
@@ -40,9 +40,7 @@ def download_youtube_video(url: str, video_output_path: str = "", convert_to_aud
 
         # converting the video file to an audio file
         print("Converting video to audio ...")
-        successful = convert_video_to_audio(path_of_video_clip, output_path=video_output_path, video_title=stream.title)
-        if not successful:
-            raise Exception("Error occurred while converting video file!")
+        convert_video_to_audio(path_of_video_clip, output_path=video_output_path, video_title=stream.title)
         print('Done converting video to mp3!')
 
         # try to delete video file after converting to audio file
@@ -90,10 +88,8 @@ def download_youtube_playlist(url: str, video_output_path: str = "", convert_to_
 
             # converting the video file to an audio file
             print("Converting video to audio ...")
-            successful = convert_video_to_audio(path_of_video_clip, output_path=video_output_path,
-                                                video_title=stream.title)
-            if not successful:
-                raise Exception("Error occurred while converting video file!")
+            convert_video_to_audio(path_of_video_clip, output_path=video_output_path,
+                                   video_title=stream.title)
             print('Done converting video to mp3!')
 
             # try to delete video file after converting to audio file
@@ -105,9 +101,9 @@ def download_youtube_playlist(url: str, video_output_path: str = "", convert_to_
 
 # function to convert a video file to an audio file mp3
 # will return true if successful otherwise false
-def convert_video_to_audio(video_path, output_path='', video_title='temp'):
+def convert_video_to_audio(video_file_path: str, output_path="", video_title='temp'):
     try:
-        clip = mp.VideoFileClip(video_path)
+        clip = mp.VideoFileClip(video_file_path)
         # setting path of audio file
         path_of_audio_file = fr"{output_path}\{video_title}.mp3"
         if output_path == '':
@@ -117,24 +113,92 @@ def convert_video_to_audio(video_path, output_path='', video_title='temp'):
         # wait till audio file is written then close clip and then can delete video in functions above
         clip.close()
     except:
-        print("Error: could not convert video file to audio file")
-        return False
-    return True
+        raise Exception("Error occurred while converting video file!")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # welcome message
     print("--------------- Running Youtube video and playlist downloader script ---------------")
-    # testing download YouTube video function by putting in download path
-    test_url1 = 'https://www.youtube.com/watch?v=81m8_5mccgA&list=PL65E33789AA7052BC&index=9'
-    test_url2 = 'https://www.youtube.com/watch?v=XzOvgu3GPwY&ab_channel=TaylorSwiftVEVO'
 
-    # testing download YouTube video by putting in current path
-    # download_youtube_video(test_url1, convert_to_audio=True)
-    # # testing download YouTube video by putting in download folder
-    # download_youtube_video(test_url2, video_output_path=DOWNLOADS_PATH, convert_to_audio=True)
+    run_script = True
+    while run_script:
+        print("Please choose an option: \n")
+        # asking user to download a YouTube video or playlist
+        print("Enter 'v': to download a YouTube video")
+        print("Enter 'p': to download a YouTube playlist")
+        print(">: ")
+        user_input = str(input())
+        print("\n")
 
-    # testing download youtube playlist
-    test_playlist_url = 'https://www.youtube.com/watch?v=EsSOv7Skl-0&list=PLEQbUq-H7Uczc3eSVDGrCnl62wzGqRzql&ab_channel=JScar'
-    download_youtube_playlist(test_playlist_url, video_output_path=DOWNLOADS_PATH, convert_to_audio=True)
+        if user_input == "v" or user_input == "V":
+            print("Please enter the url of the Youtube video >: ")
+            video_url = str(input())
+            print("\n")
+
+            print(
+                "Please enter the folder path to store the Youtube video (If nothing is entered will use current "
+                "directory)" ">: ")
+            video_path = str(input())
+            # getting rid of any leading and ending quotation marks " ' in the folder path if there are any
+            video_path = video_path.strip('"')
+            video_path = video_path.strip("'")
+            print("\n")
+
+            # asking user if they want the videos converted as mp3 files
+            print("Enter 'y': to convert YouTube video to an audio file (.mp3)")
+            print("Enter anything else to leave the youtube video as a video file ")
+            print(">: ")
+            audio_input = str(input())
+            print("\n")
+            # changing the audio bool flag depending on what the user enters
+            want_audio = False
+            if audio_input == "y" or audio_input == "Y":
+                want_audio = True
+
+            # now calling the function to download the YouTube video
+            download_youtube_video(video_url, video_path, want_audio)
+            print("\n")
+
+        elif user_input == "p" or user_input == "P":
+            print("Please enter the url of the Youtube playlist >: ")
+            playlist_url = str(input())
+            print("\n")
+
+            print(
+                "Please enter the folder path to store the Youtube videos (If nothing is entered will use current "
+                "directory)" ">: ")
+            video_path = str(input())
+            # getting rid of any leading and ending quotation marks " ' in the folder path if there are any
+            video_path = video_path.strip('"')
+            video_path = video_path.strip("'")
+            print("\n")
+
+            # asking user if they want the videos converted as mp3 files
+            print("Enter 'y': to convert YouTube videos to audio files (.mp3)")
+            print("Enter anything else to leave the youtube videos as video files ")
+            print(">: ")
+            audio_input = str(input())
+            print("\n")
+            # changing the audio bool flag depending on what the user enters
+            want_audio = False
+            if audio_input == "y" or audio_input == "Y":
+                want_audio = True
+
+            # now calling the function to download the YouTube video
+            download_youtube_playlist(playlist_url, video_path, want_audio)
+            print("\n")
+
+        else:
+            print("Please select a valid option!\n\n")
+            continue
+
+        # asking if user wants to end script or convert another video or playlist
+        print("Enter 'y': to download another YouTube video or playlist ")
+        print("Enter anything else to exit the script: ")
+        print(">: ")
+        exit_input = str(input())
+        print("\n")
+        # if the user input wants to exit then exit otherwise repeat
+        if exit_input != "y" and exit_input != "Y":
+            run_script = False
+            print("--------------- Exiting Youtube video and playlist downloader script ---------------")
